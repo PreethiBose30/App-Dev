87eb989232a5e7204bbfdf17ab2878bfc93c78aa
Preethi <preethibose.30@gmail.com>
2026-07-01 18:42:50 +0530

    Enhance HomeScreen layout and styling
    Updated HomeScreen to include a new app bar and background color, and modified text styles. Added action buttons for notifications and user profile.

--- lib/screens/dashboard/home_screen.dart
+++ lib/screens/dashboard/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../theme/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,16 +7,66 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
+
       appBar: AppBar(
-        title: const Text("Dashboard"),
+        backgroundColor: AppColors.background,
+        elevation: 0,
         centerTitle: false,
-      ),
-      body: const Center(
-        child: Text(
-          "Home Screen",
+        title: const Text(
+          "Digital Inventory",
           style: TextStyle(
-            fontSize: 24,
+            color: AppColors.textPrimary,
             fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications_none),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person_outline),
+          ),
+        ],
+      ),
+
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hello, Preethi 👋",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.textPrimary,
+                ),
+              ),
+
+              SizedBox(height: 8),
+
+              Text(
+                "Welcome back!",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: AppColors.textSecondary,
+                ),
+              ),
+
+              SizedBox(height: 30),
+
+              Text(
+                "Dashboard UI Coming Soon...",
+                style: TextStyle(
+                  color: AppColors.primary,
+                  fontSize: 18,
+                ),
+              ),
+            ],
           ),
         ),
       ),
