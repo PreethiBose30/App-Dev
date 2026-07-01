80eb32db311ab496ea0aa9186a770c1f989bfcdf
Preethi <preethibose.30@gmail.com>
2026-07-01 22:11:53 +0530

    Update home_screen.dart

--- lib/screens/dashboard/home_screen.dart
+++ lib/screens/dashboard/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../theme/app_colors.dart';
+import 'package:digital_inventory/screens/product_detail_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
