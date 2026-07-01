0537f8846522ee19c9f219568855c606a615ddbb
Preethi <preethibose.30@gmail.com>
2026-07-01 18:23:25 +0530

    Update home_screen.dart

---
+++ lib/screens/dashboard/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        centerTitle: false,
+      ),
+      body: const Center(
+        child: Text(
+          "Home Screen",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
