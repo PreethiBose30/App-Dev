2cd7c7469b3dbfe76e07a4fd499350d4d8b7ff66
Preethi <preethibose.30@gmail.com>
2026-07-01 19:17:22 +0530

    Update search_bar_widget.dart

--- lib/screens/dashboard/widgets/search_bar_widget.dart
+++ lib/screens/dashboard/widgets/search_bar_widget.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+import '../../../theme/app_colors.dart';
 
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+      decoration: BoxDecoration(
+        color: AppColors.surface,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+          hintText: "Search documents...",
+          hintStyle: TextStyle(color: AppColors.textSecondary),
+          prefixIcon: Icon(
+            Icons.search,
+            color: AppColors.textSecondary,
+          ),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.symmetric(vertical: 16),
+        ),
+        style: TextStyle(
+          color: AppColors.textPrimary,
+        ),
+      ),
+    );
+  }
+}
